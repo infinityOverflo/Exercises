6cd2b7d9f31015444d27a9372d52a3e8dd63ef4d
Alex <marinescu.alexandru40@gmail.com>
2024-01-17 18:52:06 +0200

    Implemented in Lexer getToken the String type

--- lexer/lex.py
+++ lexer/lex.py
@@ -111,10 +111,22 @@ class Lexer:
                 token = Token(lastChar + self.curChar, TokenType.NOTEQ)
             else:
                 self.abort("Expected != , instead got: !" + self.peek())
-        elif self.curChar == '\n':
-            token = Token(self.curChar, TokenType.NEWLINE)
         elif self.curChar == '\0':
             token = Token(self.curChar, TokenType.EOF)
+        elif self.curChar == '\n':
+            token = Token(self.curChar, TokenType.NEWLINE)
+        elif self.curChar == '\"':
+            self.nextChar()
+            startPos = self.curPos
+
+            while self.curChar != '\"':
+                if self.curChar == '\r' or self.curChar == '\n' or self.curChar == '\t' or self.curChar == '\\' or self.curChar == '%':
+                    self.abort("Illegal char in string")
+                else:
+                    self.nextChar()
+            
+            tokenText = self.source[startPos:self.curPos]
+            token = Token(tokenText, TokenType.STRING)
         else:
             self.abort("Unknown token: " + self.curChar)
 
--- tinycomp.py
+++ tinycomp.py
@@ -1,13 +1,15 @@
 from lexer.lex import *
 
 def main():
-    source = "+ * / - == <= >= != #_random message_\n <="
+    source = "+ * / - == <= >= != #_random message_\n <= \"Hello\" "
 
     lexer = Lexer(source)
 
     token = lexer.getToken()
     while token.kind != TokenType.EOF:
         print(token.kind)
+        if token.kind == TokenType.STRING:
+            print(token.text)
         token = lexer.getToken()
 
 main()
