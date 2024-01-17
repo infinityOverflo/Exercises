87ef9be36fda4194caa20fe37ddd3c60af3b631e
Alex <marinescu.alexandru40@gmail.com>
2024-01-17 18:12:10 +0200

    Added simple Token with TokenTypes ; Implemented Token for simple types in Lexer getToken

--- lexer/lex.py
+++ lexer/lex.py
@@ -1,3 +1,41 @@
+import enum
+
+class TokenType(enum.Enum):
+    EOF = -1
+    NEWLINE = 0
+    NUMBER = 1
+    IDENT = 2
+    STRING = 3
+    # Keywords
+    LABEL = 101
+    GOTO = 102
+    PRINT = 103
+    INPUT = 104
+    LET = 105
+    IF = 106
+    THEN = 107
+    ENDIF = 108
+    WHILE = 109
+    REPEAT = 110
+    ENDWHILE = 111
+    # Operators
+    EQ = 201
+    PLUS = 202
+    MINUS = 203
+    ASTERISK = 204
+    SLASH = 205
+    EQEQ = 206
+    NOTEQ = 207
+    LT = 208
+    LTEQ = 209
+    GT = 210
+    GTEQ = 211
+
+class Token:
+    def __init__(self, tokenText, tokenKind):
+        self.text = tokenText
+        self.kind = tokenKind
+
 class Lexer:
     def __init__(self, source):
         self.source = source + '\n'
@@ -27,18 +65,20 @@ class Lexer:
         pass
 
     def getToken(self):
+        token = None
+
         if self.curChar == '+':
-            pass
+            token = Token(self.curChar, TokenType.PLUS)
         elif self.curChar == '-':
-            pass
+            token = Token(self.curChar, TokenType.MINUS)
         elif self.curChar == '*':
-            pass
+            token = Token(self.curChar, TokenType.ASTERISK)
         elif self.curChar == '/':
-            pass
+            token = Token(self.curChar, TokenType.SLASH)
         elif self.curChar == '\n':
-            pass
+            token = Token(self.curChar, TokenType.NEWLINE)
         elif self.curChar == '\0':
-            pass
+            token = Token(self.curChar, TokenType.EOF)
         else:
             pass
 
